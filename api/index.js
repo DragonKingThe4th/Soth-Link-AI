b49dcf1eedbda1a0168306b2c0f7b84bf172690b
Ryu <134471319+DragonKingThe4th@users.noreply.github.com>
2026-08-22 09:12:52 +0530

    Set up Express server with health check endpoint
    Replaced server app with Express server and added health check endpoint.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-// api/index.js
-const app = require('../server/app.js');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.get('/api/health', (req, res) => {
+  res.json({ ok: true, message: "Debug API is working" });
+});
+
 module.exports = app;
